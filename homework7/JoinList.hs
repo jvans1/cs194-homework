38428be94460c99d4b509ca5b8906cfacd8d4f34
James Vanneman <jvannem@gmail.com>
2015-01-22 22:35:51 -0500

    wip g

--- homework7/JoinList.hs
+++ homework7/JoinList.hs
@@ -18,7 +18,24 @@ tag (Single m _)   = m
 tag (Append m _ _) = m
 
 
+sizedIndex :: (Sized a, Monoid a) => JoinList a b -> Int
+sizedIndex s = (getSize $ size $ tag s) - 1 
+
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ i _     | i < 0           = Nothing
-indexJ _ Empty                   = Nothing
-indexJ i j     | (getSize (tag j)) < i = Nothing
+indexJ i _     | i < 0              = Nothing
+indexJ _ Empty                      = Nothing
+indexJ i j     | (sizedIndex j) < i = Nothing
+indexJ i (Single m a)               = Just a
+indexJ i (Append m j1 j2) 
+  | (sizedIndex j1) > i             = indexJ i j1
+  | otherwise                       = indexJ i j2
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a  
+dropJ i j 
+  | (sizedIndex j) >= i    = Empty
+  | i == 0                 = j 
+
+{- dropJ i,/ -}
+
+
+

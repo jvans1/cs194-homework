02de7ffc0e04a54e30e2e1c66a8033fdaf9273a6
James Vanneman <jvannem@gmail.com>
2015-02-01 15:09:43 -0500

    homwork8 done!

--- homework8/Party.hs
+++ homework8/Party.hs
@@ -4,7 +4,6 @@ import Data.List
 import Data.Monoid
 import Data.Tree
 import Employee
-
 glCons :: Employee -> GuestList -> GuestList
 glCons e@(Emp{ empFun = f }) (GL l v) = GL (e:l) (f + v)
 
@@ -13,6 +12,11 @@ instance Monoid GuestList where
   mempty                     = GL [] 0 
   mappend (GL x a) (GL xs b) = GL (x ++ xs) (a + b)
 
+
+funScore :: GuestList -> Fun
+funScore (GL _ s) = s
+guests :: GuestList -> [Employee]
+guests (GL es _) = es
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun g1@(GL _ f1) g2@(GL _ f2) 
@@ -24,6 +28,10 @@ treeFold f b (Node{ subForest = [], rootLabel = a }) = f a b
 treeFold f b (Node{ subForest = xs, rootLabel = a } ) = f a $ foldl' (treeFold f) b xs
 
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel b []  = (soloList, soloList) 
+  where
+    soloList :: GuestList
+    soloList = glCons b $ GL [] 0 
 nextLevel b ggs = (bestListwithEmp, bestListWithoutEmp) 
   where
     mostFunList :: [GuestList] -> GuestList
@@ -47,5 +55,26 @@ nextLevel b ggs = (bestListwithEmp, bestListWithoutEmp)
         secondList :: [GuestList]
         secondList = map snd ggs
 
+
+
 maxFun :: Tree Employee -> GuestList
-maxFun Node{ subForest = [], rootLabel = e } = GL [e] 
+maxFun = bestList . topLists
+  where 
+    bestList :: (GuestList, GuestList) -> GuestList
+    bestList (a, b) = moreFun a b
+    topLists :: Tree Employee -> (GuestList, GuestList)
+    topLists Node{ subForest = ss, rootLabel = e } = nextLevel e $ map topLists ss
+
+
+main :: IO ()
+main = do 
+  empTree <- readFile "company.txt"
+  putStrLn $ "Total fun score: " ++ (show $ funScore $ maxFun $ readTree empTree)
+  putStrLn $ foldr (++) "" $ map nameConvert $ map (\x -> empName x ) $ guests $ maxFun $ readTree empTree
+  {- putStrLn "Done" -}
+readTree :: String -> Tree Employee
+readTree = read
+nameConvert :: Name -> String
+nameConvert = id
+
+

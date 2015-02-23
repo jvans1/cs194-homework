4d50321d1cc1a84cae7fc4523d3addca5f5e5377
James Vanneman <jvannem@gmail.com>
2015-02-22 22:38:21 -0500

    wip

---
+++ hw11/Functions.hs
@@ -0,0 +1,24 @@
+pair :: Applicative f => f a -> f b -> f (a,b)
+pair = liftA2 (,)
+{- f = Maybe: the result is Nothing if either of the arguments is; if both are Just the result is Just their pairing. -}
+{- f = []: pair computes the Cartesian product of two lists. -}
+{- f = ZipList: pair is the same as the standard zip function. -}
+{- f = IO: pair runs two IO actions in sequence, returning a pair of their results. -}
+{- f = Parser: pair runs two parsers in sequence (the parsers consume consecutive sections of the input), returning their results as a pair. If either parser fails, the whole thing fails. -}
+
+
+
+(*>) :: Applicative f => f a -> f b -> f b
+(*>) _ fb = fb
+
+
+
+mapA :: Applicative f => (a -> f b) -> ([a] -> f [b])
+
+-- Maybe
+
+
+mapA       :: Applicative f => (a -> f b) -> ([a] -> f [b])
+sequenceA  :: Applicative f => [f a] -> f [a]
+replicateA :: Applicative f => Int -> f a -> f [a]
+
